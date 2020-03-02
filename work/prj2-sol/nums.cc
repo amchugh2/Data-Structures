07a52d53f71a4cddc09b1a80e521ea5e6086d76f
Abigail McHugh <amchugh2@binghamton.edu>
2020-03-02 11:20:52 -0500

    unfinished prj2

--- work/prj2-sol/nums.cc
+++ work/prj2-sol/nums.cc
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <cstring>
+#include <istream>
 
 void main(int argc, char *argv[]){
 	// Error checking
 	if(argc < 2){
 		std::cerr << "usage: ./nums [-a] INTS_FILE_PATH\n";
+		return EXIT_FAILURE;
+	}
+
+	if(argv[1] != "-a"){
+		std::cerr << "Option can only be [-a]\n";
+		return EXIT_FAILURE;
 	}
 
 	std::ifstream in(argv[2]);
@@ -12,6 +19,18 @@ void main(int argc, char *argv[]){
 	if(!in.good(){
 		std::cerr << "cannot read " << argv[2] << ": No such file or directory\n";
 	}
+	
+	using TestType = int;
+	int i;
+	// Create seq test type
+	Seq<TestType> seq;
+	
+	while(in.good()){
+		in >> i;
+		
+
+
+       	
 
 
 }
