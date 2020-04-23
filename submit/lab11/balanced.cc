7164a3dadb05cade8728214942d22d4eea67383b
Abigail McHugh <amchugh2@binghamton.edu>
2020-04-23 16:36:11 -0400

    working lab11

--- submit/lab11/balanced.cc
+++ submit/lab11/balanced.cc
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <iostream>
 
+// NOTE: THIS PROGRAM ONLY WORKS IF ARGUMENTS ARE WITH DOUBLE QUOTES (" ") NOT SINGLE (' ')
 using namespace std;
 
 string concatenate(string &input){
@@ -18,8 +19,8 @@ string concatenate(string &input){
 }
 
 bool is_delimiter(string &input){
-	char good[] = "(,),[,],{,}";
-	size_t found = input.find_first_not_of("()[]{}");
+	//char good[] = "(,),[,],{,},<,>";
+	size_t found = input.find_first_not_of("()[]{}<>");
 	if(found != string::npos){
 		cout << "invalid delmiter" << endl;
 		return false;
@@ -29,104 +30,66 @@ bool is_delimiter(string &input){
 	}
 }
 
-
-bool is_balanced(string str){
-	stack<char> s;
-	char ch;
-
-	for(int i = 0; i < str.length(); i++){
-		if(str[i] == "(" || str[i] == "[" || str[i] == "{"){
-			s.push(str[i]);
-			continue;
-		}
-		if(s.empty()){
-			cout << "Please enter delimiters" << endl;
-			return false;
-		}
-
-		switch(str[i]){
-			case ')':
-				ch = s.top();
-				s.pop();
-				if(ch == "{" || ch == "["){
-					return false;
-				}
-				break;
-			case '}':
-				ch = s.top();
-				s.pop();
-				if(ch == "(" || ch == "["){
-					return false;
-				}
-				break;
-			case ']':
-				ch = s.top();
-				s.pop();
-				if(ch == '(' || ch == '{'){
-					return false;
-				}
-				break;
-		}
-	}
-	return (s.empty());
-}
-
-bool isBalanced(string expr) { 
+bool is_balanced(string expr) { 
     stack<char> s; 
     char ch; 
   
-    for (int i=0; i<expr.length(); i++) { //for each character in the expression, check conditions 
-        if (expr[i]=='('||expr[i]=='['||expr[i]=='{') { //when it is opening bracket, push into stack
+    for (int i=0; i<expr.length(); i++){
+        if (expr[i]=='('||expr[i]=='['||expr[i]=='{' || expr[i] == '<'){
             s.push(expr[i]); 
             continue; 
         } 
 
-        if (s.empty()) //stack cannot be empty as it is not opening bracket, there must be closing bracket
+        if (s.empty())
            return false; 
   
         switch (expr[i]){ 
-        case ')': //for closing parenthesis, pop it and check for braces and square brackets
+	case ')':
             ch = s.top(); 
             s.pop(); 
-            if (ch=='{' || ch=='[') 
+            if (ch=='{' || ch=='[' || ch == '<') 
                 return false; 
             break; 
   
-        case '}': //for closing braces, pop it and check for parenthesis and square brackets
-  
+        case '}': 
             ch = s.top(); 
             s.pop(); 
-            if (ch=='(' || ch=='[') 
+            if (ch=='(' || ch=='[' || ch == '<') 
                 return false; 
             break; 
+	
+	case '>':
+	    ch = s.top();
+	    s.pop();
+	    if(ch == '(' || ch == '[' || ch == '{')
+		    return false;
+	    break;
   
-        case ']': //for closing square bracket, pop it and check for braces and parenthesis
-  
+        case ']': 
             ch = s.top(); 
             s.pop(); 
-            if (ch =='(' || ch == '{') 
+            if (ch =='(' || ch == '{' || ch == '<') 
                 return false; 
-            break; 
-        } 
+            break;
+	}
     } 
-    return (s.empty()); //when stack is empty, return true
+    return (s.empty()); 
 } 
   
  
-main() { 
-    string expr = "([[]])";
-  
-    if (isBalanced(expr)) 
-        cout << "Balanced"; 
-    else
-        cout << "Not Balanced"; 
-}
-
+//string expr = "([[]])";
 
 int main(int argc, char *argv[]){
 	string input = string (argv[1]);
 	string str = concatenate(input);
+	cout << str << endl;
 	if(is_delimiter(str)){
-		cout << "true" << endl;
+		string expr = str;
+		if(is_balanced(expr)){
+			return 0;
+		}
+		else if(!is_balanced(expr)){
+			cout << "unbalanced" << endl;
+		}
 	}
 }
