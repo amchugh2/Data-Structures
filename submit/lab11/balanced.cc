72033cb65ebb210f7f4b6d05d2ae2ac4b432e1b1
amchugh2 <45582884+amchugh2@users.noreply.github.com>
2020-04-23 16:09:37 -0400

    Update balanced.cc

--- submit/lab11/balanced.cc
+++ submit/lab11/balanced.cc
@@ -71,6 +71,57 @@ bool is_balanced(string str){
 	return (s.empty());
 }
 
+bool isBalanced(string expr) { 
+    stack<char> s; 
+    char ch; 
+  
+    for (int i=0; i<expr.length(); i++) { //for each character in the expression, check conditions 
+        if (expr[i]=='('||expr[i]=='['||expr[i]=='{') { //when it is opening bracket, push into stack
+            s.push(expr[i]); 
+            continue; 
+        } 
+
+        if (s.empty()) //stack cannot be empty as it is not opening bracket, there must be closing bracket
+           return false; 
+  
+        switch (expr[i]){ 
+        case ')': //for closing parenthesis, pop it and check for braces and square brackets
+            ch = s.top(); 
+            s.pop(); 
+            if (ch=='{' || ch=='[') 
+                return false; 
+            break; 
+  
+        case '}': //for closing braces, pop it and check for parenthesis and square brackets
+  
+            ch = s.top(); 
+            s.pop(); 
+            if (ch=='(' || ch=='[') 
+                return false; 
+            break; 
+  
+        case ']': //for closing square bracket, pop it and check for braces and parenthesis
+  
+            ch = s.top(); 
+            s.pop(); 
+            if (ch =='(' || ch == '{') 
+                return false; 
+            break; 
+        } 
+    } 
+    return (s.empty()); //when stack is empty, return true
+} 
+  
+ 
+main() { 
+    string expr = "([[]])";
+  
+    if (isBalanced(expr)) 
+        cout << "Balanced"; 
+    else
+        cout << "Not Balanced"; 
+}
+
 
 int main(int argc, char *argv[]){
 	string input = string (argv[1]);
