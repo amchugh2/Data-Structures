1abbc0fca74cf717a118f916a3c4227830d9d147
amchugh2 <45582884+amchugh2@users.noreply.github.com>
2020-04-24 18:02:10 -0400

    Update multi-queues.cc

--- submit/prj3-sol/multi-queues.cc
+++ submit/prj3-sol/multi-queues.cc
@@ -1,150 +1,70 @@
-#include <iostream>
-
-#include <cassert>
-#include <cctype>
-#include <cstdlib>
-#include <cstring>
-
-#include "job.hh"
-
-class MaxHeap {
-public:
-  MaxHeap(Job heapArray[], int nElements, int size) :
-    heap(heapArray), nAlloc(size), nElements(nElements)
-  {
-    buildHeap();
-  }
-
-  void enqueue(Job job) {
-    assert(nElements < nAlloc && "heap is full");
-    heap[nElements++] = job; //add value at end
-    for (int pos = nElements - 1; //pos of added value
-	 pos != 0 && heap[pos] > heap[parent(pos)];
-	 pos = parent(pos)) {
-      swap(pos, parent(pos));
-    }
-  }
-
-  int dequeue() {
-    assert(nElements > 0 && "heap is empty");
-    int value = heap[0];
-    heap[0] = heap[nElements - 1]; //put last element at root
-    --nElements; //delete it
-    if (nElements > 0) siftDown(0);  //move it to correct position
-    return value;
-  }
-
-  int size() { return nElements; }
-
+// Heap class
+template <typename E, typename Comp> class heap {
 private:
-  Job* heap; 
-  int nAlloc;    //max # of elements in heap
-  int nElements; //# of currently allocated element in heap
-
-  /** Heapify heap [0, nElements). */
-  void buildHeap() {
-    //move internal nodes down if necessary
-    for (int i = nElements/2 - 1; i >= 0; --i) {
-      siftDown(i);
-    }
-  }
-
-  /** Move element at pos down in the tree until it is greater
-   *  than either of its two children.
-   *  When it is necessary to move down, prefer to move down
-   *  into subtree with the greater root. 
-   */
-  void siftDown(int pos) {
-    assert(0 <= pos && pos < nElements);
-    while (!isLeaf(pos)) {
-      int leftKidPos = leftChild(pos);
-      //there must be a left kid since pos is not a leaf
-      assert(0 <= leftKidPos && leftKidPos < nElements);
-      int rightKidPos = rightChild(pos);
-      int maxKidPos =
-	(rightKidPos >= 0 && heap[rightKidPos] > heap[leftKidPos])
-	? rightKidPos
-	: leftKidPos;
-      if (heap[pos] >= heap[maxKidPos]) return;
-      swap(pos, maxKidPos);
-      pos = maxKidPos;
-    }
-  }
-
-  void swap(int pos1, int pos2) {
-    assert(0 <= pos1 && pos1 < nElements);
-    assert(0 <= pos2 && pos2 < nElements);
-    int tmp = heap[pos1];
-    heap[pos1] = heap[pos2];
-    heap[pos2] = tmp;
-  }
-
-  //tree accessors
-  bool isLeaf(int pos) const {
-    return (pos >= nElements/2) && (pos < nElements);
-  }
-
-  int leftChild(int pos) {
-    return (2*pos + 1 < nElements) ? 2*pos + 1 : -1;
-  }
-
-  int rightChild(int pos) {
-    return (2*pos + 2 < nElements) ? 2*pos + 2 : -1;
-  }
-
-  int parent(int pos) {
-    return (pos > 0) ? (pos - 1)/2 : -1;
-  }
-
-  friend std::ostream& operator<<(std::ostream& out, const MaxHeap& heap);
+E* Heap; // Pointer to the heap array
+int maxsize; // Maximum size of the heap
+int n; // Number of elements now in the heap
+// Helper function to put element in its correct place
+void siftdown(int pos) {
+while (!isLeaf(pos)) { // Stop if pos is a leaf
+int j = leftchild(pos); int rc = rightchild(pos);
+if ((rc < n) && Comp::prior(Heap[rc], Heap[j]))
+j = rc; // Set j to greater child’s value
+if (Comp::prior(Heap[pos], Heap[j])) return; // Done
+swap(Heap, pos, j);
+pos = j; // Move down
+}
+}
+public:
+heap(E* h, int num, int max) // Constructor
+{ Heap = h; n = num; maxsize = max; buildHeap(); }
+int size() const // Return current heap size
+{ return n; }
+bool isLeaf(int pos) const // True if pos is a leaf
+{ return (pos >= n/2) && (pos < n); }
+int leftchild(int pos) const
+{ return 2*pos + 1; } // Return leftchild position
+int rightchild(int pos) const
+{ return 2*pos + 2; } // Return rightchild position
+int parent(int pos) const // Return parent position
+{ return (pos-1)/2; }
+void buildHeap() // Heapify contents of Heap
+{ for (int i=n/2-1; i>=0; i--) siftdown(i); }
+// Insert "it" into the heap
+void insert(const E& it) {
+Assert(n < maxsize, "Heap is full");
+int curr = n++;
+Heap[curr] = it; // Start at end of heap
+// Now sift up until curr’s parent > curr
+while ((curr!=0) &&
+(Comp::prior(Heap[curr], Heap[parent(curr)]))) {
+swap(Heap, curr, parent(curr));
+curr = parent(curr);
+}
+}
+Figure 5.19 An implementation for the heap.
+Sec. 5.5 Heaps and Priority Queues 181
+// Remove first value
+E removefirst() {
+Assert (n > 0, "Heap is empty");
+swap(Heap, 0, --n); // Swap first with last value
+if (n != 0) siftdown(0); // Siftdown new root val
+return Heap[n]; // Return deleted value
+}
+// Remove and return element at specified position
+E remove(int pos) {
+Assert((pos >= 0) && (pos < n), "Bad position");
+if (pos == (n-1)) n--; // Last element, no work to do
+else
+{
+swap(Heap, pos, --n); // Swap with last value
+while ((pos != 0) &&
+(Comp::prior(Heap[pos], Heap[parent(pos)]))) {
+swap(Heap, pos, parent(pos)); // Push up large key
+pos = parent(pos);
+}
+if (n != 0) siftdown(pos); // Push down small key
+}
+return Heap[n];
+}
 };
-
-std::ostream&
-operator<<(std::ostream& out, const MaxHeap& heap) {
-  out << "[ ";
-  for (int i = 0; i < heap.nElements; i++) {
-    out << heap.heap[i] << " ";
-  }
-  out << "]";
-  return out;
-}
-
-enum { MAX_HEAP_SIZE = 16 };
-
-/** prefix of  argv[] elements which are ints used to 
- *  to initialize heap; if remaining arg starts is
- *  +VAL, then VAL enqueued onto heap; if "-", then
- *  a value is dequeued from heap.
- */
-int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    std::cerr << "usage: " << argv[0] << " INT... [+INT|-]" << std::endl;
-    exit(1);							       
-  }
-  std::ostream& out = std::cout;
-  int array[MAX_HEAP_SIZE];
-  int argvIndex;
-  int nElements = 0;
-  for (argvIndex = 1; argvIndex < argc; argvIndex++) {
-    const char* arg = argv[argvIndex];
-    if (!isdigit(arg[0])) break;
-    assert(nElements < MAX_HEAP_SIZE && "too many initial elements");
-    array[nElements++] = atoi(arg);
-  }
-  MaxHeap heap(array, nElements, MAX_HEAP_SIZE);
-  out << "initial heap: " << heap << std::endl;
-  for (; argvIndex < argc; ++argvIndex) {
-    const char* arg = argv[argvIndex];
-    if (arg[0] == '+') {
-      int value = atoi(&arg[1]);
-      heap.enqueue(value);
-      out << "enqueued " << value << ": " << heap << std::endl;
-    }
-    else {
-      assert(strcmp(arg, "-") == 0 && "invalid argv[] value");
-      int value = heap.dequeue();
-      out << "dequeued " << value << ": " << heap << std::endl;
-    }
-  }
-}
-
